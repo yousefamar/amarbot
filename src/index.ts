48b262a9bb517a80cc7f68bd5a86b67c0e2bdabc
Yousef Amar <yousefamar@gmail.com>
2022-09-03 19:07:22 +0100

    refact: clean up

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,5 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
+	FOREFRONT_KEY: string;
 }
 
 const url = 'https://shared-api.forefront.link/organization/d5I0q8EinB6I/gpt-j-6b-vanilla/completions/4fVl6eeEs021';
@@ -24,7 +7,7 @@ const url = 'https://shared-api.forefront.link/organization/d5I0q8EinB6I/gpt-j-6
 export default {
 	async fetch(
 		request: Request,
-		env: Env & { FOREFRONT_KEY: string },
+		env: Env,
 		ctx: ExecutionContext
 	): Promise<Response> {
 		const headers = {
